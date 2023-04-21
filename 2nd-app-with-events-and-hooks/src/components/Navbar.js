55efa4a61eebc220b830c786aa696ae935874edf
Junaid Ansari <ansarijunaid957@gmail.com>
2023-04-21 17:52:28 +0530

    Added icon in navbar remove text from navigation bar

--- 2nd-app-with-events-and-hooks/src/components/Navbar.js
+++ 2nd-app-with-events-and-hooks/src/components/Navbar.js
@@ -6,7 +6,8 @@ export default function Navbar(props) {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">{props.heading}</a>
+                    {/* <a className="navbar-brand" href="/">{props.heading}</a> */}
+                    <a className="navbar-brand" href="/"><i class="bi bi-house-fill"></i></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
