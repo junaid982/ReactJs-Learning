ac8a28b578212913a77d8391ee49da8d1c9b58ca
Junaid Ansari <ansarijunaid957@gmail.com>
2023-04-21 17:49:06 +0530

    text area and navbar added but now implemented properly

---
+++ 2nd-app-with-events-and-hooks/src/components/TextArea.js
@@ -0,0 +1,34 @@
+import React , {useState} from 'react'
+ 
+
+
+export default function TextArea(props) {
+    let TextCapitalize = () =>{
+        alert('function working');
+    }
+
+
+
+    const [text , setText] = useState('Enter Text Here')
+    return (
+        <>
+            <div className='p-5 shadow my-5 rounded-3'>
+                <h1 className='display-5 text-center '>{props.heading}</h1>
+
+                <label htmlFor="textarea">
+                    Enter Text Below
+                </label>
+
+                <textarea cols="" rows="8" className='form-control' placeholder={text}></textarea>
+                <div className='my-3'>
+
+                    <button className='btn btn-primary' onClick={TextCapitalize} >Capitalize Text</button>
+                </div>
+
+
+
+            </div>
+
+        </>
+    )
+}
